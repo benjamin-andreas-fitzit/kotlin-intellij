183145f19b9773bf6693595b692d30b1912ef665
Benjamin Andreas Fitz <kontakt@fitz-it.de>
2022-09-05 13:46:51 +0200

    refactoring variables / constats

--- hi-welt/src/main.kt
+++ hi-welt/src/main.kt
@@ -3,14 +3,13 @@ import kotlin.math.pow
 fun main() {
 
     var width: Int = 1920
-    val height: Int = 1080
-
-    val pi: Double = Math.PI
-
     var radius: Double = 9.0
 
-    println("Total Pixels: " + width*height + " pixels.")
+    val pi: Double = Math.PI
+    val height: Int = 1080
 
+
+    println("Total Pixels: " + width*height + " pixels.")
     println("Circular area: " + pi * radius * radius + "cm².")
 
 }
