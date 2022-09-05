b57312b59c0ec984c4f74866e1b3814558252f5f
Benjamin Andreas Fitz <kontakt@fitz-it.de>
2022-09-05 13:43:10 +0200

    initial hello world commit

---
+++ hi-welt/src/main.kt
@@ -0,0 +1,16 @@
+import kotlin.math.pow
+
+fun main() {
+
+    var width: Int = 1920
+    val height: Int = 1080
+
+    val pi: Double = Math.PI
+
+    var radius: Double = 9.0
+
+    println("Total Pixels: " + width*height + " pixels.")
+
+    println("Circular area: " + pi * radius * radius + "cm².")
+
+}
