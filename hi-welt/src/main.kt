ad42e37cb398710d8c6bc5842aa68bcfdb96839d
Benjamin Andreas Fitz <kontakt@fitz-it.de>
2022-09-06 09:14:34 +0200

    adding Arrays

--- hi-welt/src/main.kt
+++ hi-welt/src/main.kt
@@ -5,10 +5,15 @@ fun main() {
     var width: Int = 1920
     var radius: Double = 9.0
 
+    var myArray = arrayOf(0, 1, 2, "Vielen Dank für die Blumen")
+    var my2ndArray = Array(10) {i -> i}
+
     val pi: Double = Math.PI
     val height: Int = 1080
-    
+
     println("Total Pixels: " + width*height + " pixels.")
     println("Circular area: " + pi * radius * radius + "cm².")
+
+    println(myArray[3])
 
 }
