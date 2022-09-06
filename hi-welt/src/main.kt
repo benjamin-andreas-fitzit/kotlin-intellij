153a091a8f218f3ae87e757d1eba838950e0e77e
Benjamin Andreas Fitz <kontakt@fitz-it.de>
2022-09-06 09:25:19 +0200

    if else

--- hi-welt/src/main.kt
+++ hi-welt/src/main.kt
@@ -8,6 +8,10 @@ fun main() {
     var myArray = arrayOf(0, 1, 2, "Vielen Dank für die Blumen")
     var my2ndArray = Array(10) {i -> i}
 
+    var a: Int = 10
+    var b: Int = 20
+    var max: Int
+
     val pi: Double = Math.PI
     val height: Int = 1080
 
@@ -15,5 +19,17 @@ fun main() {
     println("Circular area: " + pi * radius * radius + "cm².")
 
     println(myArray[3])
+
+    if (my2ndArray[2] == 2) {
+        println("es ist 2!")
+    }
+
+    if (a > b) {
+        max = a
+    } else {
+        max = b
+    }
+
+    println(max)
 
 }
