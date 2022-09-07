f4b7ce9198da96be212d7be609b3ac2d127df1cb
Benjamin Andreas Fitz <kontakt@fitz-it.de>
2022-09-07 10:46:52 +0200

    nullbare booleans

---
+++ nullable-bools/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var meinBool: Boolean? = null // null, false, true
+
+    if (meinBool == true) {
+        println(meinBool)
+    }
+    else {
+        println("false oder null")
+    }
+
+}
