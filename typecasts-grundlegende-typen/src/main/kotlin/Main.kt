9226dbbdf52812991aef170b34309d83e331b76b
Benjamin Andreas Fitz <kontakt@fitz-it.de>
2022-09-07 11:12:45 +0200

    typecasting fur anfanger

---
+++ typecasts-grundlegende-typen/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+
+    var meineVar: Int = 42
+    var meineVar2: Long = meineVar.toLong()
+    var meineVar3: Double = (meineVar/5).toDouble()
+    var meineVar4: Double = meineVar.toDouble()/5
+
+
+    println(meineVar)
+    println(meineVar2)
+    println(meineVar3)
+    println(meineVar4)
+}
