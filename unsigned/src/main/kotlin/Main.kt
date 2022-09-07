6d7115b6b7a1ba965cfba3b4c5875003d4c22f96
Benjamin Andreas Fitz <kontakt@fitz-it.de>
2022-09-07 10:35:07 +0200

    unsigned types

---
+++ unsigned/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    
+    var meinUByte: UByte = 255U // 2^8-1
+    var meinUInt: UInt = 4294967295U // 2^32-1
+    var meinULong: ULong = 18446744073709551615U // 2^64-1
+
+    println(meinUByte)
+    println(meinUInt)
+    println(meinULong)
+}
